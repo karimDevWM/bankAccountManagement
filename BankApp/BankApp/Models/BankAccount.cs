a5400988b0f271108b5459b85e6ce694f61e49ca
bastion <clbastion@laposte.net>
2022-10-12 13:38:02 +0200

    update

--- BankApp/BankApp/Models/BankAccount.cs
+++ BankApp/BankApp/Models/BankAccount.cs
@@ -5,4 +5,20 @@ public class BankAccount
     public double balance { get; set; }
     public Client clients { get; set; }
     public int accountNumber { get; set; }
+
+    public BankAccount Withdraw(BankAccount account)
+    {
+        Console.WriteLine("ToDo");
+        return account;
+    }
+    public BankAccount Deposit(BankAccount account)
+    {
+        Console.WriteLine("ToDo");
+        return account;
+    }
+    public BankAccount Transfer(BankAccount account)
+    {
+        Console.WriteLine("ToDo");
+        return account;
+    }
 }
